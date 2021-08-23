5c5009159213c3be3469c9ee192f5b5bbfe9ae60
Pavel Begunkov <asml.silence@gmail.com>
2021-08-23 11:18:44 +0100

    iov_iter: track truncated size
    [ Upstream commit 2112ff5ce0c1128fe7b4d19cfe7f2b8ce5b595fa ]
    
    Remember how many bytes were truncated and reverted back. Because
    not reexpanded iterators don't always work well with reverting, we may
    need to know that to reexpand ourselves when needed.
    
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- include/linux/uio.h
+++ include/linux/uio.h
@@ -48,6 +48,7 @@ struct iov_iter {
 			unsigned int start_head;
 		};
 	};
+	size_t truncated;
 };
 
 static inline enum iter_type iov_iter_type(const struct iov_iter *i)
@@ -256,8 +257,10 @@ static inline void iov_iter_truncate(struct iov_iter *i, u64 count)
 	 * conversion in assignement is by definition greater than all
 	 * values of size_t, including old i->count.
 	 */
-	if (i->count > count)
+	if (i->count > count) {
+		i->truncated += i->count - count;
 		i->count = count;
+	}
 }
 
 /*
@@ -266,6 +269,7 @@ static inline void iov_iter_truncate(struct iov_iter *i, u64 count)
  */
 static inline void iov_iter_reexpand(struct iov_iter *i, size_t count)
 {
+	i->truncated -= count - i->count;
 	i->count = count;
 }
 
