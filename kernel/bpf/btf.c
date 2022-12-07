ec7d5212c4fec2d454933b5f6c2263c6940ab8e5
Kumar Kartikeya Dwivedi <memxor@gmail.com>
2022-12-08 02:11:35 +0530

    bpf: Refactor ARG_PTR_TO_DYNPTR checks into process_dynptr_func
    ARG_PTR_TO_DYNPTR is akin to ARG_PTR_TO_TIMER, ARG_PTR_TO_KPTR, where
    the underlying register type is subjected to more special checks to
    determine the type of object represented by the pointer and its state
    consistency.
    
    Move dynptr checks to their own 'process_dynptr_func' function so that
    is consistent and in-line with existing code. This also makes it easier
    to reuse this code for kfunc handling.
    
    Then, reuse this consolidated function in kfunc dynptr handling too.
    Note that for kfuncs, the arg_type constraint of DYNPTR_TYPE_LOCAL has
    been lifted.
    
    Acked-by: David Vernet <void@manifault.com>
    Acked-by: Joanne Koong <joannelkoong@gmail.com>
    Signed-off-by: Kumar Kartikeya Dwivedi <memxor@gmail.com>
    Link: https://lore.kernel.org/r/20221207204141.308952-2-memxor@gmail.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    [v6.1: move changes in check_kfunc_args() in verifier.c to
    btf_check_func_arg_match() in btf.c]
    Signed-off-by: Shaoying Xu <shaoyi@amazon.com>

--- kernel/bpf/btf.c
+++ kernel/bpf/btf.c
@@ -6491,29 +6491,17 @@ static int btf_check_func_arg_match(struct bpf_verifier_env *env,
 				}
 
 				if (arg_dynptr) {
-					if (reg->type != PTR_TO_STACK) {
-						bpf_log(log, "arg#%d pointer type %s %s not to stack\n",
+					if (reg->type != PTR_TO_STACK &&
+					    reg->type != PTR_TO_DYNPTR) {
+						bpf_log(log, "arg#%d pointer type %s %s not to stack or dynptr_ptr\n",
 							i, btf_type_str(ref_t),
 							ref_tname);
 						return -EINVAL;
 					}
 
-					if (!is_dynptr_reg_valid_init(env, reg)) {
-						bpf_log(log,
-							"arg#%d pointer type %s %s must be valid and initialized\n",
-							i, btf_type_str(ref_t),
-							ref_tname);
-						return -EINVAL;
-					}
-
-					if (!is_dynptr_type_expected(env, reg,
-							ARG_PTR_TO_DYNPTR | DYNPTR_TYPE_LOCAL)) {
-						bpf_log(log,
-							"arg#%d pointer type %s %s points to unsupported dynamic pointer type\n",
-							i, btf_type_str(ref_t),
-							ref_tname);
-						return -EINVAL;
-					}
+					ret = process_dynptr_func(env, regno, ARG_PTR_TO_DYNPTR, NULL);
+				      if (ret < 0)
+					      return ret;
 
 					continue;
 				}
