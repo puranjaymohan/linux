f3f26853417cdc7175f27c9eae24c2c436e32998
Florent Revest <revest@chromium.org>
2023-03-21 15:04:18 +0100

    ftrace: Let unregister_ftrace_direct_multi() call ftrace_free_filter()
    [ Upstream commit 59495740f79524bebe4ecb8097cf206f86587b25 ]
    
    A common pattern when using the ftrace_direct_multi API is to unregister
    the ops and also immediately free its filter. We've noticed it's very
    easy for users to miss calling ftrace_free_filter().
    
    This adds a "free_filters" argument to unregister_ftrace_direct_multi()
    to both remind the user they should free filters and also to make their
    life easier.
    
    Link: https://lkml.kernel.org/r/20230321140424.345218-2-revest@chromium.org
    
    Suggested-by: Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: Florent Revest <revest@chromium.org>
    Acked-by: Mark Rutland <mark.rutland@arm.com>
    Acked-by: Jiri Olsa <jolsa@kernel.org>
    Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>

--- kernel/trace/ftrace.c
+++ kernel/trace/ftrace.c
@@ -5796,7 +5796,8 @@ EXPORT_SYMBOL_GPL(register_ftrace_direct_multi);
  *  0 on success
  *  -EINVAL - The @ops object was not properly registered.
  */
-int unregister_ftrace_direct_multi(struct ftrace_ops *ops, unsigned long addr)
+int unregister_ftrace_direct_multi(struct ftrace_ops *ops, unsigned long addr,
+				   bool free_filters)
 {
 	struct ftrace_hash *hash = ops->func_hash->filter_hash;
 	int err;
@@ -5814,6 +5815,9 @@ int unregister_ftrace_direct_multi(struct ftrace_ops *ops, unsigned long addr)
 	/* cleanup for possible another register call */
 	ops->func = NULL;
 	ops->trampoline = 0;
+
+	if (free_filters)
+		ftrace_free_filter(ops);
 	return err;
 }
 EXPORT_SYMBOL_GPL(unregister_ftrace_direct_multi);
