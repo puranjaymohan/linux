aa98153c2fc4a8df7e0e07b81a786f74ae5d6407
Puranjay Mohan <puranjay12@gmail.com>
2024-03-11 14:12:50 +0000

    selftests: bpf: Add a testcase for userspace memory access
    Add a testcase where the BPF program tries to dereference a valid
    userspace address.
    
    Signed-off-by: Puranjay Mohan <puranjay12@gmail.com>

--- tools/testing/selftests/bpf/prog_tests/subprogs_extable.c
+++ tools/testing/selftests/bpf/prog_tests/subprogs_extable.c
@@ -2,12 +2,33 @@
 
 #include <test_progs.h>
 #include "test_subprogs_extable.skel.h"
+#include <sys/mman.h>
+#include <string.h>
+
+#define BUFFER_SIZE 4096
 
 void test_subprogs_extable(void)
 {
 	const int read_sz = 456;
 	struct test_subprogs_extable *skel;
+	u64 *ptr;
 	int err;
+
+	err = write_sysctl("/proc/sys/vm/mmap_min_addr", "0");
+	if (!ASSERT_OK(err, "write_sysctl mmap_min_addr"))
+		return;
+
+	ptr = mmap(NULL, BUFFER_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANON | MAP_FIXED, -1, 0);
+	if (!ASSERT_NULL(ptr, "mmap userspace"))
+		return;
+
+	/* ptr starts at 0, the BUFFER is filled with a known value which
+	 * the BPF program will read erroneously due to the overflow. It
+	 * will put the value in skel->bss->f_version. If the JIT emits
+	 * checks for the overflow then the read should not work and the
+	 * skel->bss->f_version will be zero.
+	 */
+	memset(ptr, 0xff, BUFFER_SIZE);
 
 	skel = test_subprogs_extable__open_and_load();
 	if (!ASSERT_OK_PTR(skel, "skel_open_and_load"))
@@ -22,8 +43,15 @@ void test_subprogs_extable(void)
 
 	ASSERT_NEQ(skel->bss->triggered, 0, "verify at least one program ran");
 
+	/*
+	 * If this is not zero then the BPF program erroneously read user
+	 * memory and the JIT has a bug.
+	 */
+	ASSERT_EQ(skel->bss->f_version, 0, "verify that read from userspace address failed");
+
 	test_subprogs_extable__detach(skel);
 
 cleanup:
+	ASSERT_OK(munmap(ptr, BUFFER_SIZE), "unmap userspace ptr");
 	test_subprogs_extable__destroy(skel);
 }
--- tools/testing/selftests/bpf/progs/test_subprogs_extable.c
+++ tools/testing/selftests/bpf/progs/test_subprogs_extable.c
@@ -12,6 +12,7 @@ struct {
 } test_array SEC(".maps");
 
 unsigned int triggered;
+u64 f_version = 0;
 
 static __u64 test_cb(struct bpf_map *map, __u32 *key, __u64 *val, void *data)
 {
@@ -43,6 +44,24 @@ int BPF_PROG(handle_fexit_ret_subprogs3, int arg, struct file *ret)
 {
 	*(volatile long *)ret;
 	*(volatile int *)&ret->f_mode;
+	bpf_for_each_map_elem(&test_array, test_cb, NULL, 0);
+	triggered++;
+	return 0;
+}
+
+SEC("fexit/bpf_testmod_return_ptr")
+int BPF_PROG(handle_fexit_ret_subprogs4, int arg, struct file *ret)
+{
+	/* With arg = 3, bpf_testmod_return_ptr returns -EINVAL
+	 * -EINVAL is 0xffffffffffffffea which is an invalid address. the
+	 * JIT should add an exception table entry for the read, but
+	 * ret->f_version; will wrap and form a userspace address which
+	 * the userspace program has mapped. So the read should succeed if
+	 * the JIT doesn't emit checks for userspace addresses in
+	 * PROBE_MEM.
+	 */
+	if (ret && arg == 3)
+		f_version = ret->f_version;
 	bpf_for_each_map_elem(&test_array, test_cb, NULL, 0);
 	triggered++;
 	return 0;
